d6cc2cc7233ebb6c93f443b221528562e9b0982d
Vicki <776128986@qq.com>
2025-05-15 16:10:25 +0800

    change php

--- public/sample-index.php
+++ public/sample-index.php
@@ -1,25 +1,19 @@
 <?php
-
-/**
- * Sample HTML page
- *
- * @package   OpenEMR
- * @link      http://www.open-emr.org
- *
- * @author    Stephen Nielson <stephen@nielson.org>
- * @copyright Copyright (c) 2021 Stephen Nielson <stephen@nielson.org>
- * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
- */
-
+require_once dirname(__FILE__, 4) . '/globals.php';
+$externalUrl = "https://devcopilot.wpecker.ai/";
 ?>
+<!DOCTYPE html>
 <html>
-<title>Sample Module</title>
+<head>
+    <title>Redirect to WoodPecker Platform</title>
+</head>
 <body>
-<h1>OpenEMR Custom Skeleton Index page</h1>
-<p>Congratulations you have installed and activated your OpenEMR module.  You can replace this page with anything you would like.</p>
-
-<ul>
-    <li><a href="globals-sample.php">Globals Sample</a></li>
-</ul>
+    <h2>Redirecting to WoodPecker Platform</h2>
+    <p>You will be redirected to <a href="<?php echo $externalUrl; ?>"><?php echo $externalUrl; ?></a> in 3 seconds...</p>
+    <script>
+        setTimeout(function() {
+            window.location.href = '<?php echo $externalUrl; ?>';
+        }, 3000);
+    </script>
 </body>
 </html>
